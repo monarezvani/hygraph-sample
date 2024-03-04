e49c0912e859116cc9fff21db79e21c8146f0d04
Mona Rezvani <rezvanii.mona@gmail.com>
2024-03-04 12:06:47 +0100

    refactoring

--- services/createApolloClient.ts
+++ services/createApolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
-import { BASE_ENDPOINT } from "@/config/Endpoints";
+import { BASE_ENDPOINT } from "../config/Endpoints";
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
