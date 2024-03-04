a33c725acd033c0cd7fb66020c322bd430473bb2
Mona Rezvani <rezvanii.mona@gmail.com>
2024-03-04 12:09:51 +0100

    refactor endpoint

--- config/endpoints.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_ENDPOINT = "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clt5rv8jc04og07w4wh87mzzw/master";
--- services/createApolloClient.ts
+++ services/createApolloClient.ts
@@ -1,13 +1,12 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
-import { BASE_ENDPOINT } from "../config/Endpoints";
 
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: BASE_ENDPOINT,
+      uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clt5rv8jc04og07w4wh87mzzw/master",
     }),
   });
 });
